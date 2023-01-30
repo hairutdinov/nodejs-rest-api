33288e7f49c3d42103a778ed924f9a5b0a091b84
bulat <bulatxajrutdinoff@yandex.ru>
2023-01-30 20:25:13 +0300

    server-side validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,20 +1,43 @@
+const { validationResult } = require('express-validator')
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{
-            title: 'First post',
-            content: 'This is the first post'
-        }]
+        posts: [
+            {
+                _id: 1,
+                title: 'First post',
+                content: 'This is the first post',
+                imageUrl: 'images/book.jpeg',
+                creator: {
+                    name: 'Bulat'
+                },
+                createdAt: new Date(),
+            }
+        ]
     })
 }
 
 exports.createPost = (req, res) => {
     const { title, content } = req.body
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            message: 'Validation failed, entered data is incorrect.',
+            errors: errors.array(),
+        })
+    }
+
     res.status(201).json({
         message: 'Post created successfully!',
         post: {
-            id: new Date().toISOString(),
+            _id: new Date().toISOString(),
             title,
-            content
+            content,
+            creator: {
+                name: 'Bulat'
+            },
+            createdAt: new Date(),
         }
     })
 }
