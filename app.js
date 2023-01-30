73ac6688f563c397ab8b1349321b4e4d09dc67a2
bulat <bulatxajrutdinoff@yandex.ru>
2023-01-30 20:06:48 +0300

    cors

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express()
 // app.use(bodyParser.urlencoded()) // x-www-form-encoded <form>
 app.use(bodyParser.json()) // application/json
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
+
 app.use('/feed', feedRouter)
 
 app.listen(8180)
