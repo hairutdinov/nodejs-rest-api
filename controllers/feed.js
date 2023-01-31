c114743e6d5774ee4d80ee4811694b203ab4043a
bulat <bulatxajrutdinoff@yandex.ru>
2023-01-31 12:36:50 +0300

    updating post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator')
 const Post = require('../models/post')
+const fs = require('fs')
+const path = require('path')
 
 exports.getPosts = (req, res, next) => {
     Post
@@ -83,4 +85,63 @@ exports.getPost = (req, res, next) => {
             }
             next(e)
         })
+}
+
+exports.updatePost = (req, res, next) => {
+    const { id } = req.params
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, entered data is incorrect.')
+        error.statusCode = 422
+        throw error
+    }
+
+    const { title, content } = req.body
+    let imageUrl = req.body.image
+
+    if (req.file) {
+        imageUrl = req.file.path
+    }
+
+    if (!imageUrl) {
+        const error = new Error('No file picked')
+        error.statusCode = 422
+        throw error
+    }
+
+    Post.findById(id)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not found post')
+                error.statusCode = 404
+                throw error
+            }
+
+            if (imageUrl !== post.imageUrl) {
+                clearImage(post.imageUrl)
+            }
+
+            post.title = title
+            post.content = content
+            post.imageUrl = imageUrl
+            return post.save()
+        })
+        .then(result => {
+            res.status(200).json({
+                message: 'Post updated',
+                post: result
+            })
+        })
+        .catch(e => {
+            if (!e.statusCode) {
+                e.statusCode = 500
+            }
+            next(e)
+        })
+}
+
+const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath)
+    fs.unlink(filePath, err => console.error(err))
 }
