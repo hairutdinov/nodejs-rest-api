8fc073880e3af34cd5387dc26b99c74933f42692
bulat <bulatxajrutdinoff@yandex.ru>
2023-02-10 06:54:38 +0300

    input validation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,24 @@
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
+const validator = require('validator')
 
 module.exports = {
     createUser: async function ({ userInput }, req) {
         const { email, name, password } = userInput
+        const errors = []
+        if (!validator.isEmail(email)) {
+            errors.push({ message: 'E-mail is invalid'})
+        }
+
+        if (validator.isEmpty(password) || !validator.isLength(password, { min: 5 })) {
+            errors.push({ message: 'Password too short' })
+        }
+
+        if (errors.length > 0) {
+            const e = new Error('Invalid input')
+            throw e
+        }
+
         const existingUser = await User.findOne({ email })
         if (existingUser) {
             const e = new Error('User exists already!')
