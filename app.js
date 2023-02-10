ae31b8d580574dec6abad17d853a091f60f1c968
bulat <bulatxajrutdinoff@yandex.ru>
2023-02-10 08:37:48 +0300

    1. create post mutation 2. auth middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const multer = require('multer')
 const { graphqlHTTP } = require('express-graphql')
 const graphqlSchema = require('./graphql/schema')
 const graphqlResolvers = require('./graphql/resolvers')
+const auth = require('./middleware/auth');
 
 require ('dotenv').config()
 
@@ -49,6 +50,8 @@ app.use((req, res, next) => {
     }
     next()
 })
+
+app.use(auth);
 
 app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,9 @@
-const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 const validator = require('validator')
 const jwt = require('jsonwebtoken')
+
+const User = require('../models/user')
+const Post = require('../models/post')
 
 module.exports = {
     createUser: async function ({ userInput }, req) {
@@ -52,13 +54,61 @@ module.exports = {
             e.statusCode = 401
             throw e
         }
-        const token = jwt.sign({
-            userId: user._id.toString(),
-            email: user.email
-        }, process.env.JWT_SECRET, { expiresIn: '1h' })
+        const token = jwt.sign(
+            {
+                userId: user._id.toString(),
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        )
+        return { token: token, userId: user._id.toString() }
+    },
+    createPost: async function({ postInput }, req) {
+        if (!req.isAuth) {
+            const e = new Error('Not authenticated!')
+            e.statusCode = 401
+            throw e
+        }
+        const errors = []
+        if (
+            validator.isEmpty(postInput.title) ||
+            !validator.isLength(postInput.title, { min: 5 })
+        ) {
+            errors.push({ message: 'Title is invalid.' })
+        }
+        if (
+            validator.isEmpty(postInput.content) ||
+            !validator.isLength(postInput.content, { min: 5 })
+        ) {
+            errors.push({ message: 'Content is invalid.' })
+        }
+        if (errors.length > 0) {
+            const e = new Error('Invalid input.')
+            e.data = errors
+            e.statusCode = 422
+            throw e
+        }
+        const user = await User.findById(req.userId)
+        if (!user) {
+            const e = new Error('Invalid user.')
+            e.statusCode = 401
+            throw e
+        }
+        const post = new Post({
+            title: postInput.title,
+            content: postInput.content,
+            imageUrl: postInput.imageUrl,
+            creator: user
+        })
+        const createdPost = await post.save()
+        user.posts.push(createdPost)
+        await user.save()
         return {
-            token,
-            userId: user._id.toString()
+            ...createdPost._doc,
+            _id: createdPost._id.toString(),
+            createdAt: createdPost.createdAt.toISOString(),
+            updatedAt: createdPost.updatedAt.toISOString()
         }
     }
 }
